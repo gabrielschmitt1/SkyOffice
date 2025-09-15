1af1a3f1e767cf639a1521bf0b887aa1e6ff5d0c
Gabriel Schmitt <gabriel.schmitt@solfacil.com.br>
2025-09-15 16:28:16 -0300

    Replace Network.ts with SimpleNetwork redirect and update all dependencies

---
+++ client/src/services/Network.ts
@@ -0,0 +1,5 @@
+// Redirecionamento para SimpleNetwork para evitar erros do Colyseus
+import SimpleNetwork from './SimpleNetwork'
+
+// Exportar SimpleNetwork como Network para compatibilidade
+export default SimpleNetwork
